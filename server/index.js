3700c1b46db0665281af1612682dc722f50c94a0
Ziad-Elganzory <ziadmohamed770@outlook.com>
2025-04-26 00:52:09 +0300

    Initial Websocket Implementation

---
+++ server/index.js
@@ -0,0 +1,49 @@
+// const http = require('http').createServer();
+// const io = require('socket.io')(http,{
+//     cors: {origin:"*"}
+// })
+
+// io.on('connection', (socket)=> {
+//     console.log('a user connected')
+//     socket.on('message', (message) => {
+//         console.log('message: ' + message)
+//         io.emit('message', `${socket.id.substr(0, 2)}: ${message}`)
+//     });
+// });
+
+// http.listen(3000,()=>{
+//     console.log('listening on http://localhost:3000')
+// })
+
+
+const http = require('http');
+const WebSocket = require('ws');
+
+const server = http.createServer();
+const wss = new WebSocket.Server({ server, path: '/' });
+
+wss.on('connection', (ws) => {
+  console.log('a user connected');
+
+  ws.id = generateId();
+
+  ws.on('message', (message) => {
+    console.log('message: ' + message);
+
+    // Broadcast to all clients
+    wss.clients.forEach(client => {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(`${ws.id}: ${message}`);
+      }
+    });
+  });
+});
+
+server.listen(3000, () => {
+  console.log('listening on http://localhost:3000');
+});
+
+// Helper to generate a short random ID like socket.io ID substring
+function generateId() {
+  return Math.random().toString(36).substr(2, 2);
+}
