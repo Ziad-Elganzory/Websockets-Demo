c7ce3baeb4f0c951dc0e74fa417c52a6c1432b7c
Ziad-Elganzory <ziadmohamed770@outlook.com>
2025-04-26 00:58:34 +0300

    Remove unwanted comments

--- server/index.js
+++ server/index.js
@@ -1,21 +1,3 @@
-// const http = require('http').createServer();
-// const io = require('socket.io')(http,{
-//     cors: {origin:"*"}
-// })
-
-// io.on('connection', (socket)=> {
-//     console.log('a user connected')
-//     socket.on('message', (message) => {
-//         console.log('message: ' + message)
-//         io.emit('message', `${socket.id.substr(0, 2)}: ${message}`)
-//     });
-// });
-
-// http.listen(3000,()=>{
-//     console.log('listening on http://localhost:3000')
-// })
-
-
 const http = require('http');
 const WebSocket = require('ws');
 
